ca03d43cc52c46a361d96665f811873c7362bce8
logust79 <logust@yahoo.com>
2016-11-23 00:23:39 +0000

    wrote _update_db to handle all database update

---
+++ REAWDME.md
@@ -0,0 +1,2 @@
+## Some useful utilities used my me for UKIRDC project
+Note that the scripts depend on BioTools/CommonFuncs.py
--- lib/irdc.py
+++ lib/irdc.py
@@ -21,6 +21,18 @@ irdc_c.execute('''CREATE TABLE IF NOT EXISTS variants
 #    (id text, hpos text, exac text, kaviar_af real, cadd_phred real)''')
 irdc_conn.commit()
 
+def _update_db(table,field,value_dict):
+    # update database
+    remain_fields = list(set(['exac','kaviar_af','cadd_phred'])-set([field]))
+    for k,v in value_dict.iteritems():
+        irdc_c.execute('''INSERT OR REPLACE INTO %(table)s (id, %(field)s, %(f1)s, %(f2)s)
+            VALUES (  ?,
+            ?,
+            (SELECT %(f1)s FROM variants WHERE id = ?),
+            (SELECT %(f2)s FROM variants WHERE id = ?)
+            )''' % {'table':table,'field':field,'f1':remain_fields[0],'f2':remain_fields[1]}, (k,v,k,k))
+    irdc_conn.commit()
+
 
 def _dict_factory(cursor, row):
     # convert from sqlite tuple to dictionary
@@ -54,15 +66,19 @@ class Variant(object):
                 print 'querying the web for exac'
                 exac = anno_exac(self.cleaned_variant_id)
                 # insert into database
-                if db_var == None:
-                    irdc_c.execute('INSERT INTO variants VALUES (?,?,?,?)',(self.cleaned_variant_id,json.dumps(exac, indent=4),None,None))
-                else:
-                    irdc_c.excute('UPDATE variants SET exac=? where id=?',(json.dumps(exac, indent=4)), self.variant_id)
-                irdc_conn.commit()
+                _update_db(
+                           'variants',
+                           'exac',
+                           {self.cleaned_variant_id:json.dumps(exac,indent=4)}
+                           )
             else:
                 exac = json.loads(db_var['exac'])
             self._exac = exac
         return self._exac
+    @property
+    def kaviar_af(self):
+        self._kaviar_af = 0.
+        return self._kaviar_af
 
 class Variants:
     def __init__(self, vars=[]):
@@ -92,13 +108,14 @@ class Variants:
             if new_vars:
                 print 'querying web'
                 new_result = anno_exac_bulk(new_vars.values())
+                # update database
+                _update_db(
+                           'variants',
+                           'exac',
+                           {k:json.dumps(v,indent=4) for k,v in new_result.iteritems()}
+                           )
+                # populate exac
                 for k,v in new_vars.iteritems():
-                    irdc_c.execute('''INSERT OR REPLACE INTO variants (id, exac, kaviar_af, cadd_phred)
-                        VALUES (  ?,
-                        ?,
-                        (SELECT kaviar_af FROM variants WHERE id = ?),
-                        (SELECT cadd_phred FROM variants WHERE id = ?)
-                        )''', (v,json.dumps(new_result[v],indent=4),v,v))
                     exac[k] = new_result[v]
                 irdc_conn.commit()
         self._exac = exac
